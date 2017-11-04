76a60c8cd7091357e5b70e13507d098872ae7063
Brian <legobmw99@gmail.com>
2017-11-04 13:13:00 -0400

    Tin does some stuff

--- src/main/java/com/legobmw99/feruchemy/items/bands/TinBand.java
+++ src/main/java/com/legobmw99/feruchemy/items/bands/TinBand.java
@@ -4,6 +4,8 @@ import com.legobmw99.feruchemy.items.AbstractItemBand;
 
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 
 public class TinBand extends AbstractItemBand {
 
@@ -14,17 +16,31 @@ public class TinBand extends AbstractItemBand {
 
 	@Override
 	public void stopEffects(EntityLivingBase player) {
-		// TODO Auto-generated method stub
-
+		if(player.getActivePotionEffect(Potion.getPotionById(16)) != null && player.getActivePotionEffect(Potion.getPotionById(16)).getAmplifier() == 10){
+			player.removePotionEffect(Potion.getPotionById(16));
+		}
 	}
 
 	@Override
 	protected void bandDrainEffects(ItemStack stack, EntityLivingBase player, byte power) {
+		PotionEffect effect = new PotionEffect(Potion.getPotionById(16), Short.MAX_VALUE , 10, false, true);
+
+		player.addPotionEffect(effect);
+		
+		if (player.isPotionActive(Potion.getPotionById(15))) {
+			player.removePotionEffect(Potion.getPotionById(15));
+		}
 	}
 
 	@Override
 	protected void bandFillEffects(ItemStack stack, EntityLivingBase player, byte power) {
+		PotionEffect effect = new PotionEffect(Potion.getPotionById(15), 40 , 0, false, true);
+		player.addPotionEffect(effect);
 		
+		if(power == 3 && !player.isPotionActive(Potion.getPotionById(9))){
+			PotionEffect effect2 = new PotionEffect(Potion.getPotionById(9), 100 , 0, false, true);
+			player.addPotionEffect(effect2);
+		}
 	}
 
 	@Override
