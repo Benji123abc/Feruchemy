c4ff1bd5c3daff8c0c0e1bd098aea72a720d48f5
Ben <34463636+Benji123abc@users.noreply.github.com>
2021-03-04 10:41:21 -0500

    Improve IronBand weightlessness effect
    I tested this with elytras and it actually worked remarkably well, I don't think any additional behavior is required.

--- src/main/java/com/legobmw99/feruchemy/items/bands/IronBand.java
+++ src/main/java/com/legobmw99/feruchemy/items/bands/IronBand.java
@@ -5,6 +5,8 @@ import com.legobmw99.feruchemy.items.AbstractItemBand;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.SharedMonsterAttributes;
 import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Potion;
+import net.minecraft.potion.PotionEffect;
 
 public class IronBand extends AbstractItemBand {
 
@@ -14,7 +16,6 @@ public class IronBand extends AbstractItemBand {
 
 	@Override
 	public void stopEffects(EntityLivingBase player) {
-		player.setNoGravity(false);
 		player.getEntityAttribute(SharedMonsterAttributes.KNOCKBACK_RESISTANCE).setBaseValue(0);
 	}
 
@@ -22,26 +23,21 @@ public class IronBand extends AbstractItemBand {
 	protected void bandDrainEffects(ItemStack stack, EntityLivingBase player, byte power) {
 		player.motionY += (0.04D * power / 3.0) ;
 		player.velocityChanged = true;
-		
 	}
 
 	@Override
 	protected void bandFillEffects(ItemStack stack, EntityLivingBase player, byte power) {
 		player.fallDistance = 0.0F;
-		if(player.isElytraFlying() && power == 3){
-			// TODO: something
-		}
-
+		player.motionY += (0.04D * power / 3.0);
+		player.velocityChanged = true;
+		player.addPotionEffect(new PotionEffect(Potion.getPotionById(1), 10 , power*2, false, true));
 	}
 
 	@Override
 	protected void beginFillEffect(EntityLivingBase player, int power) {
-		if (player.world.isRemote) {
-			return;
-		}
-		if(power == 3){
-			player.setNoGravity(true);
-		}
+//		if (player.world.isRemote) {
+//			return;
+//		}
 	}
 
 	@Override
